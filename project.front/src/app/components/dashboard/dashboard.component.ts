13a356d4955aab294601d8767bbe2396fa6b8d8d
Juan Sebastian <juannino907@gmail.com>
2024-07-29 15:49:22 -0500

    dashboard config

--- project.front/src/app/components/dashboard/dashboard.component.ts
+++ project.front/src/app/components/dashboard/dashboard.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsersService } from '../../services/users.service';
+import { usersApi } from '../../interfaces/usersInterface';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrls: ['./dashboard.component.css'] 
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  
+  userList: usersApi[] = [];
 
+  constructor(private usersService: UsersService) { }
+
+  ngOnInit(): void {
+    this.getData();
+  }
+
+  getData(): void {
+    this.usersService.getData().subscribe({
+      next: (result: usersApi[]) => { 
+        this.userList = result; 
+      },
+      error: (err) => {
+        console.error('Error fetching data', err);
+      }
+    });
+  }
 }

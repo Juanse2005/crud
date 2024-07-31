53d3cf0d0fa6f681df6eb252dcd448b3a814a667
Juan Sebastian <juannino907@gmail.com>
2024-07-31 10:57:27 -0500

    save settings

--- project.front/src/app/components/add-edit-user/add-edit-user.component.ts
+++ project.front/src/app/components/add-edit-user/add-edit-user.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { usersApi } from '../../interfaces/usersInterface';
+import { UsersService } from '../../services/users.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-edit-user',
@@ -12,20 +14,30 @@ import { usersApi } from '../../interfaces/usersInterface';
 export class AddEditUserComponent {
   addUser: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private usersService: UsersService, private router: Router) {
     this.addUser = this.fb.group({
       username: ['', Validators.required],
       password: ['', Validators.required],
     });
   }
 
-  add() {
-    console.log(this.addUser);  
+  add(): void {
+    if (this.addUser.valid) {
+      const userApi: usersApi = {
+        username: this.addUser.get('username')?.value,
+        password: this.addUser.get('password')?.value
+      };
 
-    const userApi: usersApi = {
-      username: this.addUser.get('username')?.value,
-      password: this.addUser.get('password')?.value
+      this.usersService.saveData(userApi).subscribe({
+        next: () => {
+          this.router.navigate(['/dashboard']);
+        },
+        error: (err) => {
+          console.error('Error fetching data', err);
+        }
+      });
+    } else {
+      console.error('Invalid form');
     }
-    console.log(userApi)
   }
 }

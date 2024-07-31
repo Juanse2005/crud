2cff9e9458b7dfa99bbfe5a6ffcdbdb0a127d217
Juan Sebastian <juannino907@gmail.com>
2024-07-31 08:57:29 -0500

    Add-user form

--- project.api/project.api/Program.cs
+++ project.api/project.api/Program.cs
@@ -1,16 +1,32 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using project.api.Context;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-// builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
-//CONEXION BASE DE DATOS
+// Configuración de Swagger/OpenAPI
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+});
+
+// Configuración de CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin",
+        policy =>
+        {
+            // Reemplaza con la URL de tu frontend
+            policy.WithOrigins("http://localhost:4200") // La URL de tu frontend
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
+
+// Configuración de la base de datos
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseMySql(builder.Configuration.GetConnectionString("connectionmysql"),
         new MySqlServerVersion(new Version(8, 0, 39))));
@@ -23,6 +39,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+// Habilitar CORS
+app.UseCors("AllowSpecificOrigin");
 
 app.UseAuthorization();
 
--- project.front/src/app/app.routes.ts
+++ project.front/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AddUserComponent } from './components/add-user/add-user.component';
 
 export const routes: Routes = [ 
     {path: 'login', component: LoginComponent},
     {path: 'sign-up', component: SignUpComponent},
-    {path: 'dashboard', component: DashboardComponent}
+    {path: 'dashboard', component: DashboardComponent},
+    {path: 'add-user', component: AddUserComponent}
 ];

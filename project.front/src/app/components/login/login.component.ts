7b50bdc5e5ce15dfd5f82770cc299c13b09f49e8
Juan Sebastian <juannino907@gmail.com>
2024-08-10 10:18:15 -0500

    replaced username field for email

--- project.front/src/app/components/login/login.component.ts
+++ project.front/src/app/components/login/login.component.ts
@@ -1,36 +1,44 @@
 import { Component } from '@angular/core';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { usersApi } from '../../interfaces/usersInterface';
+import { CommonModule } from '@angular/common';
 import { UsersService } from '../../services/users.service';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']  
 })
 export class LoginComponent {
   loginForm: FormGroup;
-  constructor(private fb: FormBuilder, private usersService: UsersService, private router: Router) {
+
+  constructor(
+    private fb: FormBuilder,
+    private usersService: UsersService,
+    private router: Router
+  ) {
     this.loginForm = this.fb.group({
-      username: ['', Validators.required],
-      password: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]], 
+      password: ['', Validators.required]
     });
   }
-  ngOnInit(): void {
-  }
+
+  ngOnInit(): void {}
+
   login(): void {
     if (this.loginForm.valid) {
       const userApi: usersApi = {
-        username: this.loginForm.get('username')?.value,
+        email: this.loginForm.get('email')?.value,
         password: this.loginForm.get('password')?.value
       };
-  
+
       this.usersService.login(userApi).subscribe({
         next: (response) => {
           console.log('Login successful', response);
+          // Navegar al dashboard sin recargar la página
           this.router.navigate(['/dashboard']);
         },
         error: (err) => {
@@ -39,9 +47,7 @@ export class LoginComponent {
         }
       });
     } else {
-      console.error('Invalid form');
+      alert('Invalid form');
     }
   }
-  
-  
 }

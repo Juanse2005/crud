8b35520ec0aefe215cb505b7ea67c8622821f137
Juan Sebastian <juannino907@gmail.com>
2024-08-12 09:49:54 -0500

    Dependencies

--- project.front/src/app/app.config.ts
+++ project.front/src/app/app.config.ts
@@ -6,6 +6,7 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 
 
+
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }),
   provideRouter(routes, withViewTransitions()), provideClientHydration(), provideHttpClient(withFetch())]
--- project.front/src/app/app.routes.ts
+++ project.front/src/app/app.routes.ts
@@ -1,17 +1,11 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
-import { SignUpComponent } from './components/sign-up/sign-up.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { AddEditUserComponent } from './components/add-edit-user/add-edit-user.component';
-import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [ 
-    {path: ' ', component: HomeComponent},
-    {path: 'login', component: LoginComponent},
-    {path: 'sign-up', component: SignUpComponent},
-    {path: 'dashboard', component: DashboardComponent},
-    {path: 'add-user', component: AddEditUserComponent },
-    {path: 'edit-user/:id', component: AddEditUserComponent },
-    {path: 'home', component: HomeComponent },
-    { path: '**', redirectTo: '/home', pathMatch: 'full' }
+export const routes: Routes = [
+  { path: 'home', loadComponent: () => import('./components/home/home.component').then(c => c.HomeComponent) },
+  //{ path: 'login', loadComponent: () => import('./components/login/login.component').then(c => c.LoginComponent) },
+  //{ path: 'sign-up', loadComponent: () => import('./components/sign-up/sign-up.component').then(c => c.SignUpComponent) },
+  { path: 'dashboard', loadComponent: () => import('./components/dashboard/dashboard.component').then(c => c.DashboardComponent) },
+  { path: 'add-user', loadComponent: () => import('./components/add-edit-user/add-edit-user.component').then(c => c.AddEditUserComponent) },
+  { path: 'edit-user/:id', loadComponent: () => import('./components/add-edit-user/add-edit-user.component').then(c => c.AddEditUserComponent) },
+  { path: '**', redirectTo: '/home', pathMatch: 'full' }
 ];

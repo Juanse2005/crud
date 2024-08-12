8f0345c12e5a41081f6d72c3cce88d3614d31594
Juan Sebastian <juannino907@gmail.com>
2024-08-12 09:47:42 -0500

    Navbar complete

--- project.front/src/app/components/nav/nav.component.ts
+++ project.front/src/app/components/nav/nav.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [],
+  imports: [ RouterModule,CommonModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
